4809b794be76bba8f1a66c14448a051c13f98e38
Sokoya Philip <philo4u2c@gmail.com>
2020-06-28 18:09:39 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -65,6 +65,7 @@ app.post('/ussd-requests', (req, res) => {
 app.put('/ussd-requests/:id', (req, res) => {
   const { result } = req.body
   const id = req.params.id
+  console.log( req )
 
   for(let i in db) {
     if(db[i].id.toString() === id) {
